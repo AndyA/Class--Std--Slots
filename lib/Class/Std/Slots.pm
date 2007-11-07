940461374719ceb8e64f8eacdef9f4ad76b571c5
Andy Armstrong <andy@hexten.net>
2007-11-07 14:11:05 +0000

    Tagging 0.2, version bump

--- lib/Class/Std/Slots.pm
+++ lib/Class/Std/Slots.pm
@@ -5,7 +5,7 @@ use strict;
 use Carp;
 use Scalar::Util qw(blessed refaddr weaken);
 
-use version; our $VERSION = qv( '0.1' );
+our $VERSION = '0.3';
 
 my %signal_map  = ();    # maps id -> signame -> array of connected slots
 my %signal_busy = ();    # maps id -> signame -> busy flag
@@ -326,7 +326,7 @@ Class::Std::Slots - Provide signals and slots for standard classes.
 
 =head1 VERSION
 
-This document describes Class::Std::Slots version 0.1
+This document describes Class::Std::Slots version 0.3
 
 =head1 SYNOPSIS
 
